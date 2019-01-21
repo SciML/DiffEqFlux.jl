3bdc620151685cac1ef96372fa146db9350227d4
Mike Innes <mike.j.innes@gmail.com>
2019-01-21 11:49:39 +0000

    utils

--- src/DiffEqFlux.jl
+++ src/DiffEqFlux.jl
@@ -3,6 +3,7 @@ module DiffEqFlux
 using DiffEqBase, Flux, DiffResults, DiffEqSensitivity, ForwardDiff
 
 include("Flux/layers.jl")
+include("Flux/utils.jl")
 
 export diffeq_fd, diffeq_rd, diffeq_adjoint
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,8 @@
+using DiffEqFlux, Test
+
+@testset "DiffEqFlux" begin
+
 include("layers.jl")
+include("utils.jl")
+
+end
