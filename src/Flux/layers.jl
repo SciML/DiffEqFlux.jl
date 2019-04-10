b65d3426aef0684602d40194a5f45124cc5ccce6
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-04-10 10:37:43 -0400

    fix out of place type conversions

--- src/Flux/layers.jl
+++ src/Flux/layers.jl
@@ -11,7 +11,7 @@ function diffeq_rd(p,prob,args...;u0=prob.u0,kwargs...)
       _prob = remake(prob,u0=convert.(recursive_bottom_eltype(p),u0),p=p)
     else
       # use TrackedArray for efficiency of the tape
-      _prob = remake(prob,u0=convert(recursive_bottom_eltype(p),u0),p=p)
+      _prob = remake(prob,u0=convert(typeof(p),u0),p=p)
     end
   else # u0 is functional, ignore the change
     _prob = remake(prob,u0=u0,p=p)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
-using DiffEqFlux, Test
+using DiffEqFlux, Test, SafeTestsets
 
-@testset "DiffEqFlux" begin
-
-include("layers.jl")
-include("utils.jl")
-include("neural_de.jl")
-include("partial_neural.jl")
-
-end
+@safetestset "Utils Tests" begin include("utils.jl") end
+@safetestset "Layers Tests" begin include("layers.jl") end
+@safetestset "Neural DE Tests" begin include("neural_de.jl") end
+@safetestset "Partial Neural Tests" begin include("partial_neural.jl") end
