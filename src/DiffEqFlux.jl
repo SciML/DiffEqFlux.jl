5838a734fc7d04c566d9fe26f55d07099482467a
Christopher Rackauckas <accounts@chrisrackauckas.com>
2019-04-11 19:20:50 -0400

    Update DiffEqFlux.jl

--- src/DiffEqFlux.jl
+++ src/DiffEqFlux.jl
@@ -1,7 +1,7 @@
 module DiffEqFlux
 
 using DiffEqBase, Flux, DiffResults, DiffEqSensitivity, ForwardDiff,
-      Requires, RecursiveArrayTools
+      Requires, RecursiveArrayTools, Adapt
 
 gpu_or_cpu(x) = Array
 function __init__()
