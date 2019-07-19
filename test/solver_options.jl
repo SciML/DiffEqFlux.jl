1d71b84e30d8e1635f3fb6bb1bd6cf0c802cad54
jessebett <jessebett@cs.toronto.edu>
2019-07-19 16:23:18 -0400

    Test common solver options

---
+++ test/solver_options.jl
@@ -0,0 +1,12 @@
+using OrdinaryDiffEq, StochasticDiffEq, Flux, DiffEqFlux, Test
+
+xs = Float32.(hcat([0.; 0.], [1.; 0.], [2.; 0.]))
+tspan = (0.0f0,25.0f0)
+dudt = Chain(Dense(2,50,tanh),Dense(50,2))
+
+@testset "only end" begin
+    only_end = neural_ode(dudt,xs,tspan,Tsit5(),save_everystep=false,save_start=false)
+    @test size(only_end)[end]==1
+end
+
+
