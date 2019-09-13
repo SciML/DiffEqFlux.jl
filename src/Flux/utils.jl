c63eeb3c12d5cfaa4a8955f41dc21444e5150400
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-09-13 07:53:24 -0400

    add mapleaves

--- src/Flux/utils.jl
+++ src/Flux/utils.jl
@@ -1,3 +1,8 @@
+function mapleaves(f, x; cache = IdDict())
+  haskey(cache, x) && return cache[x]
+  cache[x] = isleaf(x) ? f(x) : mapchildren(x -> mapleaves(f, x, cache = cache), x)
+end
+
 function destructure(m)
   xs = []
   mapleaves(m) do x
