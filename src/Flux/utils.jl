68a966941e977adabedceca724f759b6c7f1d1e1
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-12-28 19:00:20 +0530

    add restructure with sizes

--- src/Flux/utils.jl
+++ src/Flux/utils.jl
@@ -1,25 +1,19 @@
-using Flux.Zygote
-
-function params_(m)
-  i = 0
-  Flux.fmap(m) do x
-    x isa AbstractArray && (i += 1)
+function destructure(m)
+  xs = []
+  Flux.mapleaves(m) do x
+    x isa AbstractArray && push!(xs, x)
     return x
   end
-  xs = Zygote.Buffer(Vector(undef, i))
-  i = 1
-  Flux.fmap(m) do x
-    if x isa AbstractArray
-      xs[i] = x
-      i += 1
-    end
-    x
-  end
-  copy(xs)
+  return vcat(vec.(xs)...)
 end
 
-function destructure(m)
-  vcat(vec.(params_(m))...)
+ZygoteRules.@adjoint function destructure(m)
+  xs = []
+  Flux.mapleaves(m) do x
+    x isa AbstractArray && push!(xs, x)
+    return x
+  end
+  vcat(vec.(xs)...),ybar->(nothing,)
 end
 
 function restructure(m, xs)
@@ -30,4 +24,15 @@ function restructure(m, xs)
     i += length(x)
     return x
   end
+end
+
+function restructure(sz::Tuple, xs)
+  t = []
+  i = 0
+  for s in sz
+    y = reshape(xs[i.+(1:prod(s))], s)
+    push!(t,y)
+    i += length(y)
+  end
+  t
 end
