bfe3d9158aaeb4875d837af7897d08cef9487360
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-01-14 13:21:59 -0500

    upstream fix to test

--- test/neural_de.jl
+++ test/neural_de.jl
@@ -38,11 +38,9 @@ grads = Zygote.gradient(()->sum(node(x)),Flux.params(x,node))
 @test ! iszero(grads[x])
 @test ! iszero(grads[node.p])
 
-@test_broken begin
-    grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
-    @test ! iszero(grads[xs])
-    @test ! iszero(grads[node.p])
-end
+grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
+@test ! iszero(grads[xs])
+@test ! iszero(grads[node.p])
 
 # Adjoint
 @testset "adjoint mode" begin
