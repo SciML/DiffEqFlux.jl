0d4fab6afd207acab00105c4c51c9028f52ceba0
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-02-05 10:45:02 -0500

    fix a bunch of GPU tests

--- test/neural_de_gpu.jl
+++ test/neural_de_gpu.jl
@@ -1,7 +1,7 @@
 using OrdinaryDiffEq, StochasticDiffEq, Flux, DiffEqSensitivity, DiffEqFlux, Zygote, Test, CuArrays
 CuArrays.allowscalar(false)
 
-mp = Float32[0.1,0.1] |> gpu
+mp = Chain(Dense(2,2)) |> gpu
 x = Float32[2.; 0.] |> gpu
 xs = Float32.(hcat([0.; 0.], [1.; 0.], [2.; 0.])) |> gpu
 tspan = (0.0f0,25.0f0)
@@ -24,14 +24,14 @@ grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
 @test ! iszero(grads[xs])
 @test ! iszero(grads[node.p])
 
-@test_broken node = NeuralODE(dudt,tspan,Tsit5(),save_everystep=false,save_start=false,sensealg=TrackerAdjoint())
-#grads = Zygote.gradient(()->sum(Array(node(x))),Flux.params(x,node))
-#@test ! iszero(grads[x])
-#@test ! iszero(grads[node.p])
+node = NeuralODE(dudt,tspan,Tsit5(),save_everystep=false,save_start=false,sensealg=TrackerAdjoint())
+grads = Zygote.gradient(()->sum(Array(node(x))),Flux.params(x,node))
+@test ! iszero(grads[x])
+@test ! iszero(grads[node.p])
 
-#grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
-#@test ! iszero(grads[xs])
-#@test ! iszero(grads[node.p])
+grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
+@test ! iszero(grads[xs])
+@test ! iszero(grads[node.p])
 
 node = NeuralODE(dudt,tspan,Tsit5(),save_everystep=false,save_start=false,sensealg=BacksolveAdjoint())
 grads = Zygote.gradient(()->sum(node(x)),Flux.params(x,node))
@@ -53,7 +53,7 @@ grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
     @test ! iszero(grads[xs])
     @test ! iszero(grads[node.p])
 
-    NeuralODE(dudt,tspan,Tsit5(),saveat=0.0:0.1:10.0)
+    node = NeuralODE(dudt,tspan,Tsit5(),saveat=0.0:0.1:10.0)
     grads = Zygote.gradient(()->sum(node(x)),Flux.params(x,node))
     @test ! iszero(grads[x])
     @test ! iszero(grads[node.p])
@@ -62,24 +62,23 @@ grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
     @test ! iszero(grads[xs])
     @test ! iszero(grads[node.p])
 
-    node = NeuralODE(dudt,tspan,Tsit5(),saveat=0.1)
-    @test_broken grads = Zygote.gradient(()->sum(node(x)),Flux.params(x,node)) isa Tuple
-    #@test ! iszero(grads[x])
-    #@test ! iszero(grads[node.p])
+    node = NeuralODE(dudt,tspan,Tsit5(),saveat=1f-1)
+    grads = Zygote.gradient(()->sum(node(x)),Flux.params(x,node))
+    @test ! iszero(grads[x])
+    @test ! iszero(grads[node.p])
 
-    #grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
-    #@test ! iszero(grads[xs])
-    #@test ! iszero(grads[node.p])
+    grads = Zygote.gradient(()->sum(node(xs)),Flux.params(xs,node))
+    @test ! iszero(grads[xs])
+    @test ! iszero(grads[node.p])
 end
 
-@test_broken NeuralDMSDE(dudt,mp,(0.0f0,2.0f0),SOSRI(),saveat=0.0:0.1:2.0)(x)
-#=
-sode = NeuralDMSDE(dudt,mp,(0.0f0,2.0f0),SOSRI(),saveat=0.0:0.1:2.0)
-grads = Zygote.gradient(()->sum(sode(x)),Flux.params(x,sode))
+NeuralDSDE(dudt,mp,(0.0f0,2.0f0),SOSRI(),saveat=0.0:0.1:2.0)(x)
+sode = NeuralDSDE(dudt,mp,(0.0f0,2.0f0),SOSRI(),saveat=Float32.(0.0:0.1:2.0),dt=1f-1)
+@test_broken grads = Zygote.gradient(()->sum(sode(x)),Flux.params(x,sode)) isa Tuple
 @test ! iszero(grads[x])
-@test_broken ! iszero(grads[sode.p])
+@test ! iszero(grads[sode.p])
 
 grads = Zygote.gradient(()->sum(sode(xs)),Flux.params(xs,sode))
 @test ! iszero(grads[xs])
-@test_broken ! iszero(grads[sode.p])
+@test ! iszero(grads[sode.p])
 =#
