dc662766edd378cc5542f20d9f11e8e8c143581f
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-09 22:50:14 -0400

    [ci skip] add some prints for Travis

--- test/newton_neural_ode.jl
+++ test/newton_neural_ode.jl
@@ -11,6 +11,7 @@ training_data = Iterators.cycle([(x_data[:, i], y_data[:, i]) for i in 1:d])
 NN = Chain(Dense(n, 10n, tanh),
            Dense(10n, n))
 
+@info "ROCK4"
 nODE = NeuralODE(NN, tspan, ROCK4(), reltol=1e-4, saveat=[tspan[end]])
 
 loss_function(θ, x, y) = Flux.mse(y, nODE(x, θ))
@@ -20,12 +21,14 @@ res = DiffEqFlux.sciml_train(loss_function, nODE.p, NewtonTrustRegion(), trainin
 NN = FastChain(FastDense(n, 10n, tanh),
                FastDense(10n, n))
 
+@info "ROCK2"
 nODE = NeuralODE(NN, tspan, ROCK2(), reltol=1e-4, saveat=[tspan[end]])
 
 loss_function(θ, x, y) = Flux.mse(y, nODE(x, θ))
 res = DiffEqFlux.sciml_train(loss_function, nODE.p, LBFGS(), training_data)
 res = DiffEqFlux.sciml_train(loss_function, nODE.p, NewtonTrustRegion(), training_data)
 
+@info "ROCK4"
 nODE = NeuralODE(NN, tspan, ROCK4(), reltol=1e-4, saveat=[tspan[end]])
 
 loss_function(θ, x, y) = Flux.mse(y, nODE(x, θ))
