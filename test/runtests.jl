1f26804c9215ef5d5ffb1db287115a836c75aba2
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-05-20 18:32:29 -0400

    split test groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ const is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
 const is_TRAVIS = haskey(ENV,"TRAVIS")
 
 @time begin
-if GROUP == "All"
+if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"
     @safetestset "Layers Tests" begin include("layers.jl") end
     @safetestset "Fast Layers" begin include("fast_layers.jl") end
     @safetestset "Layers SciML Tests" begin include("layers_sciml.jl") end
@@ -14,13 +14,15 @@ if GROUP == "All"
     @safetestset "Size Handling in Adjoint Tests" begin include("size_handling_adjoint.jl") end
     @safetestset "odenet" begin include("odenet.jl") end
     @safetestset "GDP Regression Tests" begin include("gdp_regression_test.jl") end
+end
+
+if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "NeuralDE"
     @safetestset "Neural DE Tests" begin include("neural_de.jl") end
     @safetestset "Newton Neural ODE Tests" begin include("newton_neural_ode.jl") end
     @safetestset "Neural ODE MM Tests" begin include("neural_ode_mm.jl") end
     @safetestset "Neural Second Order ODE Tests" begin include("second_order_ode.jl") end
     @safetestset "Fast Neural ODE Tests" begin include("fast_neural_ode.jl") end
     @safetestset "Partial Neural Tests" begin include("partial_neural.jl") end
-end
 end
 
 if !is_APPVEYOR && GROUP == "GPU"
