ede37243d2ad3105eb8d42176d55a9a3ca2704e4
Diogo Netto <diogonetto.dcn@gmail.com>
2020-06-08 23:06:51 -0400

    Support for multivariate/Tests

--- src/ffjord.jl
+++ src/ffjord.jl
@@ -5,34 +5,28 @@ struct FFJORD{M,P,RE,ContinuousUnivariateDistribution,Bool,T,A,K} <: CNFLayer
     model::M
     p::P
     re::RE
-    baseDist::ContinuousUnivariateDistribution
-    MonteCarlo::Bool
+    basedist::Union{ContinuousUnivariateDistribution,ContinuousMultivariateDistribution}
+    monte_carlo::Bool
     tspan::T
     args::A
     kwargs::K
 
-    function FFJORD(model,tspan,args...;p = nothing,baseDist=nothing,MonteCarlo=nothing,kwargs...)
+    function FFJORD(model,tspan,args...;p = nothing,basedist= Normal(0,1),monte_carlo=false,kwargs...)
         _p,re = Flux.destructure(model)
         if p === nothing
             p = _p
         end
-        if baseDist === nothing
-            baseDist = Normal(0,1)
-        end
-        if MonteCarlo === nothing
-            MonteCarlo = false
-        end
-        new{typeof(model),typeof(p),typeof(re),typeof(baseDist),
-            typeof(MonteCarlo),typeof(tspan),typeof(args),typeof(kwargs)}(
-            model,p,re,baseDist,MonteCarlo,tspan,args,kwargs)
+        new{typeof(model),typeof(p),typeof(re),typeof(basedist),
+            typeof(monte_carlo),typeof(tspan),typeof(args),typeof(kwargs)}(
+            model,p,re,basedist,monte_carlo,tspan,args,kwargs)
     end
 end
 
-function ffjord(du,u,p,t,re,MonteCarlo,e)
+function ffjord(du,u,p,t,re,monte_carlo,e)
     z = @view u[1:end-1]
     m = re(p)
     fz, back = Zygote.pullback(m,z')
-    if MonteCarlo
+    if monte_carlo
         eJ = back(e)[1]
         jac = -(eJ.*e)[1]
     else
@@ -42,15 +36,15 @@ function ffjord(du,u,p,t,re,MonteCarlo,e)
     du[end] = jac
 end
 
-function (n::FFJORD)(x,p=n.p,MonteCarlo=n.MonteCarlo)
-    e = MonteCarlo ? randn(Float32,size(p)[1]) : nothing
-    ffjord_ = (du,u,p,t)->ffjord(du,u,p,t,n.re,MonteCarlo,e)
+function (n::FFJORD)(x,p=n.p,monte_carlo=n.monte_carlo)
+    e = monte_carlo ? randn(Float32,size(p)[1]) : nothing
+    ffjord_ = (du,u,p,t)->ffjord(du,u,p,t,n.re,monte_carlo,e)
     prob = ODEProblem{true}(ffjord_,nothing,n.tspan,nothing)
     pred = concrete_solve(prob,Tsit5(),[x,0f0],p,n.args...;n.kwargs...)[:,end]
-    pz = n.baseDist
+    pz = n.basedist
     z = pred[1]
     delta_logp = pred[2]
-    logpz = logpdf(pz, z)
+    logpz = logpdf.(pz, z)
     logpx = logpz - delta_logp
     return logpx
 end
---
+++ test/cnf_test.jl
@@ -0,0 +1,51 @@
+using Distributions
+using DifferentialEquations
+using Flux, DiffEqFlux
+using Zygote, Tracker
+using Test
+using Distances
+
+#test for default distribution and monte_carlo = false
+nn = Chain(Dense(1, 1, tanh))
+data_train = [Float32(rand(Beta(7,7))) for i in 1:100]
+tspan = (0.0,10.0)
+ffjord_test_mc = FFJORD(nn,tspan,monte_carlo=true)
+
+function loss_adjoint(θ)
+    logpx = [ffjord_test_mc(x,θ) for x in data_train]
+    loss = -mean(logpx)
+end
+
+res = DiffEqFlux.sciml_train(loss_adjoint, ffjord_test_mc.p,
+                                        ADAM(0.1),
+                                        maxiters = 100)
+
+θopt = res.minimizer
+data_validate = [Float32(rand(Beta(7,7))) for i in 1:100]
+actual_pdf = [pdf(Beta(7,7),r) for r in data_validate]
+#use direct trace calculation for predictions
+learned_pdf = [exp(ffjord_test_mc(r,θopt,false)) for r in data_validate]
+
+@test totalvariation(learned_pdf, actual_pdf)/100 < 0.3
+
+#test for alternative distribution and monte_carlo = false
+nn = Chain(Dense(1, 1, tanh))
+data_train = [Float32(rand(Normal(6.0,0.7))) for i in 1:100]
+tspan = (0.0,10.0)
+ffjord_test = FFJORD(nn,tspan,base_dist=Normal(0,2))
+
+function loss_adjoint(θ)
+    logpx = [ffjord_test(x,θ) for x in data_train]
+    loss = -mean(logpx)
+end
+
+res = DiffEqFlux.sciml_train(loss_adjoint, ffjord_test.p,
+                                          ADAM(0.1),
+                                          maxiters = 100)
+
+θopt = res.minimizer
+data_validate = [Float32(rand(Normal(6.0,0.7))) for i in 1:100]
+actual_pdf = [pdf(Normal(6.0,0.7),r) for r in data_validate]
+learned_pdf = [exp(ffjord_test(r, θopt)) for r in data_validate]
+
+@test totalvariation(learned_pdf, actual_pdf)/100 < 0.2
