effef8f736fc19ea00e5638f649f192440f4dd47
Diogo Netto <diogonetto.dcn@gmail.com>
2020-06-15 22:37:11 -0400

    Tensor Product Layer [WIP]

---
+++ src/tensor_product_basis.jl
@@ -0,0 +1,42 @@
+abstract type TensorProductBasis <: Function end
+
+##TODO: Chebyshev Basis
+
+##Fourier basis of the form F_j(x) = j is odd ? cos((j÷2)*x) : sin((j÷2)*x) => [F_0(x), F_1(x), ..., F_n(x)]
+struct FourierBasis{Int} <: TensorProductBasis
+    n::Int
+
+    function FourierBasis(n)
+        new{typeof(n)}(n)
+    end
+end
+
+function (basis::FourierBasis)(x)
+    return Fourier()[x, 1:basis.n]
+end
+
+##Legendre basis of the form [P_0(x), P_1(x), ..., P_n(x)]
+struct LegendreBasis{Int} <: TensorProductBasis
+    n::Int
+
+    function LegendreBasis(n)
+        new{typeof(n)}(n)
+    end
+end
+
+function (basis::LegendreBasis)(x)
+    return Legendre()[x, 1:basis.n]
+end
+
+##Polynomial basis of them form [x, x^2, ..., x^n]
+struct PolynomialBasis{Int} <: TensorProductBasis
+    n::Int
+
+    function PolynomialBasis(n)
+        new{typeof(n)}(n)
+    end
+end
+
+function (basis::PolynomialBasis)(x)
+    return [evalpoly(x, (I+zeros(basis.n,basis.n))[k, :]) for k in 1:basis.n]
+end
---
+++ src/tensor_product_layer.jl
@@ -0,0 +1,29 @@
+abstract type TensorProducLayer <: Function end
+
+struct TPLayer{Array({TensorProductBasis}),S<:AbstractArray,b<:AbstractArray,Int} <: TensorProducLayer
+    model::Array({TensorProductBasis})
+    W::S
+    b::T
+    in::Int
+    out::Int
+    function TPLayer(in, out, model,W=nothing,b=nothing)
+        number_of_weights = 1
+        for basis in model:
+            number_of_weights = number_of_weights*basis.n
+        end
+        W = [[zeros(number_of_weights)] for k in 1:out]
+        b = [[0f0] for k in 1:out]
+        new{typeof(W),typeof(b),typeof(in),typeof(out)}(W,b,in,out)
+    end
+end
+
+function (layer::TPLayer)(x)
+    z = []
+    tensor_prod = model[1](x)
+    for i in 2:end
+        tensor_prod = kron(tensor_prod, model[i](x))
+    end
+    for i in 1:out
+        push!(z, sum(W[i].*tensor_prod)+b[i])
+    return z
+end
