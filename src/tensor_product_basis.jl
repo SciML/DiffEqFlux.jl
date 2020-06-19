fee05e61440c7fd0dd5cab5c83d96435f23d3710
Diogo Netto <diogonetto.dcn@gmail.com>
2020-06-18 23:25:48 -0400

    no parameterization in structs

--- src/tensor_product_basis.jl
+++ src/tensor_product_basis.jl
@@ -1,11 +1,8 @@
 abstract type TensorProductBasis <: Function end
 
 ##Chebyshev basis of the form [T_0(x), T_1(x), ..., T_n(x)] where T is the Chebyshev polynomial of the first kind
-struct ChebyshevBasis{Int} <: TensorProductBasis
+struct ChebyshevBasis <: TensorProductBasis
     n::Int
-    function ChebyshevBasis(n)
-        new{typeof(n)}(n)
-    end
 end
 
 function (basis::ChebyshevBasis)(x)
@@ -13,11 +10,8 @@ function (basis::ChebyshevBasis)(x)
 end
 
 ##Fourier basis of the form F_j(x) = j is odd ? cos((j÷2)*x) : sin((j÷2)*x) => [F_0(x), F_1(x), ..., F_n(x)]
-struct FourierBasis{Int} <: TensorProductBasis
+struct FourierBasis <: TensorProductBasis
     n::Int
-    function FourierBasis(n)
-        new{typeof(n)}(n)
-    end
 end
 
 function (basis::FourierBasis)(x)
@@ -25,11 +19,8 @@ function (basis::FourierBasis)(x)
 end
 
 ##Legendre basis of the form [P_0(x), P_1(x), ..., P_n(x)]
-struct LegendreBasis{Int} <: TensorProductBasis
+struct LegendreBasis <: TensorProductBasis
     n::Int
-    function LegendreBasis(n)
-        new{typeof(n)}(n)
-    end
 end
 
 ##Source: https://github.com/ranocha/PolynomialBases.jl/blob/master/src/legendre.jl
@@ -56,11 +47,8 @@ function (basis::LegendreBasis)(x)
 end
 
 ##Polynomial basis of them form [x, x^2, ..., x^n]
-struct PolynomialBasis{Int} <: TensorProductBasis
+struct PolynomialBasis <: TensorProductBasis
     n::Int
-    function PolynomialBasis(n)
-        new{typeof(n)}(n)
-    end
 end
 
 function (basis::PolynomialBasis)(x)
