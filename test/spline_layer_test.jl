1cb6f06443a2da8ad3c3551cefe450ff9859b209
Diogo Netto <diogonetto.dcn@gmail.com>
2020-07-09 22:40:28 -0400

    spline: more tests

--- test/spline_layer_test.jl
+++ test/spline_layer_test.jl
@@ -32,14 +32,31 @@ function run_test(f, layer, atol)
     return sum(abs.(data_validate_pred.-data_validate_fn))/length(data_validate_fn) < atol
 end
 
-##test 01: affine function, Chebyshev and Polynomial basis
+ZygoteRules.@adjoint ZygoteRules.literal_getproperty(A::Tridiagonal, ::Val{:dl}) = A.dl,y -> Tridiagonal(dl,zeros(length(d)),zeros(length(du)),)
+ZygoteRules.@adjoint ZygoteRules.literal_getproperty(A::Tridiagonal, ::Val{:d}) = A.d,y -> Tridiagonal(zeros(length(dl)),d,zeros(length(du)),)
+ZygoteRules.@adjoint ZygoteRules.literal_getproperty(A::Tridiagonal, ::Val{:du}) = A.dl,y -> Tridiagonal(zeros(length(dl)),zeros(length(d),du),)
+ZygoteRules.@adjoint Tridiagonal(dl, d, du) = Tridiagonal(dl, d, du), p̄ -> (diag(p̄[2:end,1:end-1]),diag(p̄),diag(p̄[1:end-1,2:end]))
+
+##test 01: affine function, Linear Interpolation
 a, b = rand(2)
 f = x -> a*x + b
 layer = SplineLayer((0.0,1.0),0.01,LinearInterpolation)
 @test run_test(f, layer, 0.05)
 
-##test 02: non-linear function, Chebyshev and Legendre basis
+##test 02: non-linear function, Quadratic Interpolation
 a, b, c = rand(3)
 f = x -> a*x^2+ b*x + x
 layer = SplineLayer((0.0,1.0),0.01,QuadraticInterpolation)
+@test run_test(f, layer, 0.05)
+
+##test 03: non-linear function, Quadratic Spline
+a, b, c = rand(3)
+f = x -> a*sin(b*x+c)
+layer = SplineLayer((0.0,1.0),0.1,QuadraticSpline)
+@test run_test(f, layer, 0.05)
+
+##test 03: non-linear function, Cubic Spline
+a, b, c = rand(3)
+f = x -> exp(x)*x^2
+layer = SplineLayer((0.0,1.0),0.1,CubicSpline)
 @test run_test(f, layer, 0.05)
