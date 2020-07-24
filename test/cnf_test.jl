93fc88b1335236f42b51a1873102a6782b365b9f
Diogo Netto <diogonetto.dcn@gmail.com>
2020-07-24 17:52:50 -0400

    monte-carlo switch in ffjord

--- test/cnf_test.jl
+++ test/cnf_test.jl
@@ -95,27 +95,25 @@ learned_pdf = [exp(cnf_test(r, θopt)) for r in data_validate]
 #test for default multivariate distribution and FFJORD with regularizers
 ###
 
-nn = Chain(Dense(1, 1, tanh))
-data_train = [Float32(rand(Beta(7,7))) for i in 1:100]
+nn = Chain(Dense(1, 3, tanh), Dense(3, 1, tanh))
+data_train = [Float32(rand(Normal(6.0,0.7))) for i in 1:100]
 tspan = (0.0,10.0)
 ffjord_test = FFJORDLayer(nn,tspan,Tsit5())
 
 function loss_adjoint(θ)
-    logpx = [ffjord_test(x,θ)[1] for x in data_train]
-    reg1 = [ffjord_test(x,θ)[2] for x in data_train]
-    reg2 = [ffjord_test(x,θ)[3] for x in data_train]
-    loss = -mean(logpx) + 0.01*mean(reg1) + 0.01*mean(reg2)
+    logpx = [-ffjord_test(x,θ)[1] + 0.01*(ffjord_test(x,θ)[2]+ffjord_test(x,θ)[3]) for x in data_train]
+    loss = mean(logpx)
 end
 
 res = DiffEqFlux.sciml_train(loss_adjoint, ffjord_test.p,
                                         ADAM(0.01), cb=cb,
-                                        maxiters = 50)
+                                        maxiters = 100)
 
 
 θopt = res.minimizer
 data_validate = [Float32(rand(Beta(7,7))) for i in 1:100]
 actual_pdf = [pdf(Beta(7,7),r) for r in data_validate]
 #use direct trace calculation for predictions
-learned_pdf = [exp(ffjord_test(r,θopt)[1]) for r in data_validate]
+learned_pdf = [exp(ffjord_test(r,θopt,false)[1]) for r in data_validate]
 
 @test totalvariation(learned_pdf, actual_pdf)/100 < 0.25
