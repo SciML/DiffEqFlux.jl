d1687d903c87be1f46793725ddb1b167f1af6383
Diogo Netto <diogonetto.dcn@gmail.com>
2020-07-24 15:32:24 -0400

    two separate layers in ffjord.jl

--- src/ffjord.jl
+++ src/ffjord.jl
@@ -1,11 +1,35 @@
 abstract type CNFLayer <: Function end
 Flux.trainable(m::CNFLayer) = (m.p,)
 
+struct DeterministicCNFLayer{M,P,RE,Distribution,T,A,K} <: CNFLayer
+    model::M
+    p::P
+    re::RE
+    basedist::Distribution
+    tspan::T
+    args::A
+    kwargs::K
+
+    function DeterministicCNFLayer(model,tspan,args...;p = nothing,basedist=nothing,kwargs...)
+        _p,re = Flux.destructure(model)
+        if p === nothing
+            p = _p
+        end
+        if basedist === nothing
+            size_input = size(model[1].W)[2]
+            basedist = MvNormal(zeros(size_input), I + zeros(size_input,size_input))
+        end
+        new{typeof(model),typeof(p),typeof(re),typeof(basedist),typeof(tspan),typeof(args),typeof(kwargs)}(
+            model,p,re,basedist,tspan,args,kwargs)
+    end
+end
+
 """
 Constructs a continuous-time recurrent neural network, also known as a neural
 ordinary differential equation (neural ODE), with fast gradient calculation
 via adjoints [1] and specialized for density estimation based on continuous
-normalizing flows (CNF) [2]. At a high level this corresponds to the following steps:
+normalizing flows (CNF) [2] with a stochastic approach [2] for the computation of the trace
+of the dynamics' jacobian. At a high level this corresponds to the following steps:
 
 1. Parameterize the variable of interest x(t) as a function f(z,θ,t) of a base variable z(t) with known density p_z;
 2. Use the transformation of variables formula to predict the density p_x as a function of the density p_z and the trace of the Jacobian of f;
@@ -19,7 +43,6 @@ FFJORD(model,basedist=nothing,monte_carlo=false,tspan,args...;kwargs...)
 Arguments:
 - `model`: A Chain neural network that defines the ̇x.
 - `basedist`: Distribution of the base variable. Set to the unit normal by default.
-- `monte_carlo`: Method for calculating the trace of the Jacobian. The default monte_carlo = false calculates the Jacobian and its trace directly. monte_carlo = true uses the stochastic approach presented in [3] to provide an unbiased estimate for the trace.
 - `tspan`: The timespan to be solved on.
 - `kwargs`: Additional arguments splatted to the ODE solver. See the
   [Common Solver Arguments](https://diffeq.sciml.ai/dev/basics/common_solver_opts/)
@@ -30,17 +53,16 @@ Ref
 [3]W. Grathwohl, R. T. Q. Chen, J. Bettencourt, I. Sutskever, D. Duvenaud. FFJORD: Free-Form Continuous Dynamic For Scalable Reversible Generative Models. arXiv preprint at arXiv1810.01367, 2018.
 
 """
-struct FFJORD{M,P,RE,Distribution,Bool,T,A,K} <: CNFLayer
+struct FFJORDLayer{M,P,RE,Distribution,T,A,K} <: CNFLayer
     model::M
     p::P
     re::RE
     basedist::Distribution
-    monte_carlo::Bool
     tspan::T
     args::A
     kwargs::K
 
-    function FFJORD(model,tspan,args...;p = nothing,basedist=nothing,monte_carlo=false,kwargs...)
+    function FFJORDLayer(model,tspan,args...;p = nothing,basedist=nothing,kwargs...)
         _p,re = Flux.destructure(model)
         if p === nothing
             p = _p
@@ -49,9 +71,8 @@ struct FFJORD{M,P,RE,Distribution,Bool,T,A,K} <: CNFLayer
             size_input = size(model[1].W)[2]
             basedist = MvNormal(zeros(size_input), I + zeros(size_input,size_input))
         end
-        new{typeof(model),typeof(p),typeof(re),typeof(basedist),
-            typeof(monte_carlo),typeof(tspan),typeof(args),typeof(kwargs)}(
-            model,p,re,basedist,monte_carlo,tspan,args,kwargs)
+        new{typeof(model),typeof(p),typeof(re),typeof(basedist),typeof(tspan),typeof(args),typeof(kwargs)}(
+            model,p,re,basedist,tspan,args,kwargs)
     end
 end
 
@@ -61,25 +82,30 @@ function jacobian_fn(f, x::AbstractVector)
    vcat([transpose(back(ȳ(i))[1]) for i = 1:length(y)]...)
  end
 
-function ffjord(du,u,p,t,re,monte_carlo,e)
+function cnf(du,u,p,t,re)
     z = @view u[1:end-1]
     m = re(p)
-    if monte_carlo
-        _, back = Zygote.pullback(m,z)
-        eJ = back(e)[1]
-        trace_jac = (eJ.*e)[1]
-    else
-        J = jacobian_fn(m, z)
-        trace_jac = length(z) == 1 ? sum(J) : tr(J)
-    end
+    J = jacobian_fn(m, z)
+    trace_jac = length(z) == 1 ? sum(J) : tr(J)
     du[1:end-1] = m(z)
     du[end] = -trace_jac
 end
 
-function (n::FFJORD)(x,p=n.p,monte_carlo=n.monte_carlo)
-    e = monte_carlo ? randn(Float32,length(x)) : nothing
-    ffjord_ = (du,u,p,t)->ffjord(du,u,p,t,n.re,monte_carlo,e)
-    prob = ODEProblem{true}(ffjord_,vcat(x,0f0),n.tspan,p)
+function ffjord(du,u,p,t,re,e)
+    z = @view u[1:end-3]
+    m = re(p)
+    _, back = Zygote.pullback(m,z)
+    eJ = back(e)[1]
+    trace_jac = (eJ.*e)[1]
+    du[1:end-3] = m(z)
+    du[end-2] = -trace_jac
+    du[end-1] = norm(m(z))^2
+    du[end] = norm(eJ)^2
+end
+
+function (n::DeterministicCNFLayer)(x,p=n.p)
+    cnf_ = (du,u,p,t)->cnf(du,u,p,t,n.re)
+    prob = ODEProblem{true}(cnf_,vcat(x,0f0),n.tspan,p)
     pred = solve(prob,n.args...;n.kwargs...)[:,end]
     pz = n.basedist
     z = pred[1:end-1]
@@ -87,4 +113,19 @@ function (n::FFJORD)(x,p=n.p,monte_carlo=n.monte_carlo)
     logpz = log.(pdf(pz, z))
     logpx = logpz .- delta_logp
     return logpx[1]
+end
+
+function (n::FFJORDLayer)(x,p=n.p)
+    e = randn(Float32,length(x))
+    ffjord_ = (du,u,p,t)->ffjord(du,u,p,t,n.re,e)
+    prob = ODEProblem{true}(ffjord_,vcat(x,0f0,0f0,0f0),n.tspan,p)
+    pred = solve(prob,n.args...;n.kwargs...)[:,end]
+    pz = n.basedist
+    z = pred[1:end-3]
+    delta_logp = pred[end-2]
+    reg1 = pred[end-1]
+    reg2 =  pred[end]
+    logpz = log.(pdf(pz, z))
+    logpx = logpz .- delta_logp
+    return logpx[1], reg1, reg2
 end
