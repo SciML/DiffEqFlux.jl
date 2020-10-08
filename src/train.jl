8e33c57d76a8acc5f3f07b72bd3eef34e5d5e1b9
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2020-10-08 15:07:19 +0530

    Remove kwargs splatting in OptimizationFunction

--- src/train.jl
+++ src/train.jl
@@ -5,7 +5,7 @@ Base.length(::NullData) = 0
 
 function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType; kwargs...)
   @warn("sciml_train has been deprecated in favor of GalacticOptim.jl (https://github.com/SciML/GalacticOptim.jl)") 
-  optfunc = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype; kwargs...)
+  optfunc = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype)
   optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
   GalacticOptim.solve(optprob, opt; kwargs...)
 end
