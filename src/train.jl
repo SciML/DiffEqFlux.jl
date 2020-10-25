6a2622899f2470008d9ddfd465f3a133a302552c
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2020-10-25 15:57:11 +0530

    Use instantiate_function

--- src/train.jl
+++ src/train.jl
@@ -1,6 +1,6 @@
 function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(); kwargs...)
   @warn("sciml_train has been deprecated in favor of GalacticOptim.jl (https://github.com/SciML/GalacticOptim.jl)") 
-  optfunc = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype)
+  optfunc = GalacticOptim.instantiate_function((x, p) -> loss(x), θ, adtype)
   optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
   GalacticOptim.solve(optprob, opt; kwargs...)
 end
