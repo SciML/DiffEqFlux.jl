605ebebd63a07537686fee0525d8c17bfa3df8a6
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2020-10-25 21:59:26 +0530

    pass nothing for p in instantiate_function

--- src/train.jl
+++ src/train.jl
@@ -1,6 +1,6 @@
-function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(); kwargs...)
+function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(), args...; kwargs...)
   @warn("sciml_train has been deprecated in favor of GalacticOptim.jl (https://github.com/SciML/GalacticOptim.jl)") 
-  optfunc = GalacticOptim.instantiate_function((x, p) -> loss(x), θ, adtype)
+  optfunc = GalacticOptim.instantiate_function((x, p) -> loss(x), θ, adtype, nothing)
   optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
-  GalacticOptim.solve(optprob, opt; kwargs...)
+  GalacticOptim.solve(optprob, opt, args...; kwargs...)
 end
