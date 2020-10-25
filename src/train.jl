c498560c013524992c7454f95e172ddd639155b1
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2020-10-25 22:49:07 +0530

    Wrap before instantiating

--- src/train.jl
+++ src/train.jl
@@ -1,6 +1,7 @@
 function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(), args...; kwargs...)
   @warn("sciml_train has been deprecated in favor of GalacticOptim.jl (https://github.com/SciML/GalacticOptim.jl)") 
-  optfunc = GalacticOptim.instantiate_function((x, p) -> loss(x), θ, adtype, nothing)
+  optf = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype)
+  optfunc = GalacticOptim.instantiate_function(optf, θ, adtype, nothing)
   optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
   GalacticOptim.solve(optprob, opt, args...; kwargs...)
 end
