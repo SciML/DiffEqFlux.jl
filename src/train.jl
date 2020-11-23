a53592f1f8241f9b01b8bfa76d6849997203841c
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-11-23 04:15:24 -0500

    Update train.jl

--- src/train.jl
+++ src/train.jl
@@ -1,5 +1,4 @@
 function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(), args...; kwargs...)
-  @warn("sciml_train has been deprecated in favor of GalacticOptim.jl (https://github.com/SciML/GalacticOptim.jl)") 
   optf = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype)
   optfunc = GalacticOptim.instantiate_function(optf, θ, adtype, nothing)
   optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
