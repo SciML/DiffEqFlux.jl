2eb0b7e459d73488bf6a783c140efe6618942894
Avik Pal <avikpal@iitk.ac.in>
2021-01-12 12:06:36 +0530

    [WIP] Extend collocation methods to use DataInterpolations.jl (#380)
    * Extend collocation methods to use DataInterpolations.jl
    
    * Use derivatives from DataInterpolations.jl itself
    
    * Typo: not smoothed estimate
    
    * Update spline_layer_test.jl
    
    * Update spline_layer_test.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- src/DiffEqFlux.jl
+++ src/DiffEqFlux.jl
@@ -1,7 +1,7 @@
 module DiffEqFlux
 
-using GalacticOptim, DiffEqBase, DiffResults, DiffEqSensitivity, Distributions, ForwardDiff,
-      Flux, Requires, Adapt, LinearAlgebra, RecursiveArrayTools,
+using GalacticOptim, DataInterpolations, DiffEqBase, DiffResults, DiffEqSensitivity,
+      Distributions, ForwardDiff, Flux, Requires, Adapt, LinearAlgebra, RecursiveArrayTools,
       StaticArrays, Base.Iterators, Printf, Zygote
 
 using DistributionsAD
