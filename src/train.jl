d23fa129b22e5561d64825245ceaa2dfd7b85d53
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2021-03-04 06:44:25 -0500

    fix bounds translation (#499)
    * fix bounds translation
    
    fixes https://github.com/SciML/DiffEqFlux.jl/issues/498
    
    * patch release

--- src/train.jl
+++ src/train.jl
@@ -1,6 +1,7 @@
-function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(), args...; kwargs...)
+function sciml_train(loss, θ, opt, adtype::DiffEqBase.AbstractADType = GalacticOptim.AutoZygote(), args...;
+                     lower_bounds = nothing, upper_bounds = nothing, kwargs...)
   optf = GalacticOptim.OptimizationFunction((x, p) -> loss(x), adtype)
   optfunc = GalacticOptim.instantiate_function(optf, θ, adtype, nothing)
-  optprob = GalacticOptim.OptimizationProblem(optfunc, θ; kwargs...)
+  optprob = GalacticOptim.OptimizationProblem(optfunc, θ; lb = lower_bounds, ub = upper_bounds, kwargs...)
   GalacticOptim.solve(optprob, opt, args...; kwargs...)
 end
