8f16f48352f2297d9d6b3165728666c00061d6b7
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-03-31 08:25:13 -0400

    Disable neural graph differential equation tests
    Requires https://github.com/yuehhua/GeometricFlux.jl/pull/166

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,7 +27,7 @@ end
 if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "BasicNeuralDE"
     @safetestset "Neural DE Tests" begin include("neural_de.jl") end
     @safetestset "Augmented Neural DE Tests" begin include("augmented_nde.jl") end
-    @safetestset "Neural Graph DE" begin include("neural_gde.jl") end
+    #@safetestset "Neural Graph DE" begin include("neural_gde.jl") end
     @safetestset "Hybrid DE" begin include("hybrid_de.jl") end
     @safetestset "Neural ODE MM Tests" begin include("neural_ode_mm.jl") end
     @safetestset "Fast Neural ODE Tests" begin include("fast_neural_ode.jl") end
