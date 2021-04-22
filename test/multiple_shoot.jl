fcd9a50090ad27670a5a6979e33a9e73aa1a4793
Adrian Hill <adrian.hill@mailbox.org>
2021-04-22 15:41:11 +0200

    Update tests and add tests for group_ranges

--- test/multiple_shoot.jl
+++ test/multiple_shoot.jl
@@ -1,11 +1,14 @@
 using DiffEqFlux, OrdinaryDiffEq, Flux, Optim, Test
+using DiffEqFlux: group_ranges
 
-# General loss function to compare single shooting and multiple shooting predictions
-function general_loss_function(result_neuralode)
-	return sum(abs2, (ode_data[:,:] .- Array(prob_neuralode(u0, result_neuralode.minimizer)) ))
-end
+## Test group partitioning helper function
+@test group_ranges(10, 4) == [1:4, 4:7, 7:10]
+@test group_ranges(10, 5) == [1:5, 5:9, 9:10]
+@test group_ranges(10, 10) == [1:10]
+@test_throws DomainError group_ranges(10, 1)
+@test_throws DomainError group_ranges(10, 11)
 
-# Define initial conditions and timesteps
+## Define initial conditions and time steps
 datasize = 30
 u0 = Float32[2.0, 0.0]
 tspan = (0.0f0, 5.0f0)
@@ -20,63 +23,56 @@ prob_trueode = ODEProblem(trueODEfunc, u0, tspan)
 ode_data = Array(solve(prob_trueode, Tsit5(), saveat = tsteps))
 
 # Define the Neural Network
-dudt2 = FastChain((x, p) -> x.^3,
-                  FastDense(2, 16, tanh),
-                  FastDense(16, 2))
-prob_neuralode = NeuralODE(dudt2, (0.0,5.0), Tsit5(), saveat = tsteps)
+nn = FastChain((x, p) -> x.^3,
+                FastDense(2, 16, tanh),
+                FastDense(16, 2))
+p_init = initial_params(nn)
 
-function loss_neuralode(p)
-    pred = Array(prob_neuralode(u0, p))
-    loss = sum(abs2, (ode_data[:,1:size(pred,2)] .- pred))
-    return loss, pred
+neuralode = NeuralODE(nn, tspan, Tsit5(), saveat = tsteps)
+prob_node = ODEProblem((u,p,t)->nn(u,p), u0, tspan, p_init)
+
+function predict_single_shooting(p)
+    return Array(neuralode(u0, p))
 end
 
+# Define loss function
+function loss_function(data, pred)
+	return sum(abs2, data - pred)
+end
 
-result_neuralode = DiffEqFlux.sciml_train(loss_neuralode, prob_neuralode.p,
+## Evaluate Single Shooting
+function loss_single_shooting(p)
+    pred = predict_single_shooting(p)
+    l = loss_function(ode_data, pred)
+    return l, pred
+end
+
+res_single_shooting = DiffEqFlux.sciml_train(loss_single_shooting, neuralode.p,
                                           ADAM(0.05),
 										  maxiters = 300)
 
-single_shoot_loss = general_loss_function(result_neuralode)
-println("single_shoot_loss: ",single_shoot_loss)
+loss_ss, _ = loss_single_shooting(res_single_shooting.minimizer)
+println("Single shooting loss: $(loss_ss)")
 
-# Define parameters for Multiple Shooting
-grp_size_param = 1
-loss_multiplier_param = 100
+## Test Multiple Shooting
+group_size = 2
+continuity_term = 100
 
-neural_ode_f(u,p,t) = dudt2(u,p)
-prob_param = ODEProblem(neural_ode_f, u0, tspan, initial_params(dudt2))
-
-function loss_function_param(ode_data, pred):: Float32
-	return sum(abs2, (ode_data .- pred))^2
+function loss_multiple_shooting(p)
+    return multiple_shoot(p, ode_data, tsteps, prob_node, loss_function, Tsit5(),
+                          group_size; continuity_term)
 end
 
-function loss_neuralode_param(p)
-	return multiple_shoot(p, ode_data, tsteps, prob_param, loss_function_param, Tsit5(), grp_size_param, loss_multiplier_param)
-end
+res_ms = DiffEqFlux.sciml_train(loss_multiple_shooting, neuralode.p,
+                                ADAM(0.05), maxiters = 300)
 
+# Calculate single shooting loss with parameter from multiple_shoot training
+loss_ms, _ = loss_single_shooting(res_ms.minimizer)
+println("Multiple shooting loss: $(loss_ms)")
+@test loss_ms < loss_ss
 
-multiple_shoot_result_neuralode_1 = DiffEqFlux.sciml_train(loss_neuralode_param, prob_neuralode.p,
-                                          ADAM(0.05),
-                                          maxiters = 300)
-
-multiple_shoot_loss_1 = general_loss_function(multiple_shoot_result_neuralode_1)
-println("multiple_shoot_loss_1: ",multiple_shoot_loss_1)
-
-
-# test for grp_size = 1
-@test multiple_shoot_loss_1 < single_shoot_loss
-
-# test for grp_size = 5
-grp_size_param = 5
-multiple_shoot_result_neuralode_2 = DiffEqFlux.sciml_train(loss_neuralode_param, prob_neuralode.p,
-                                          ADAM(0.05),
-                                          maxiters = 300)
-
-multiple_shoot_loss_2 = general_loss_function(multiple_shoot_result_neuralode_2)
-println("multiple_shoot_loss_2: ",multiple_shoot_loss_2)
-
-@test multiple_shoot_loss_2 < single_shoot_loss
-
-# test for DomainErrors
-@test_throws DomainError multiple_shoot(prob_neuralode.p, ode_data, tsteps, prob_param, loss_function_param, Tsit5(), 0, loss_multiplier_param)
-@test_throws DomainError multiple_shoot(prob_neuralode.p, ode_data, tsteps, prob_param, loss_function_param, Tsit5(), datasize + 1, loss_multiplier_param)
+# Test for DomainErrors
+@test_throws DomainError multiple_shoot(p_init, ode_data, tsteps, prob_node,
+                                        loss_function, Tsit5(), 1)
+@test_throws DomainError multiple_shoot(p_init, ode_data, tsteps, prob_node,
+                                        loss_function, Tsit5(), datasize + 1)
