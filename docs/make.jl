3b9f6174e859bf56db852c04e7e89c2e0fbc1517
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 07:04:20 -0400

    separate out pages in docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,7 @@
 using Documenter, DiffEqFlux
 
+include("pages.jl")
+
 makedocs(
     sitename = "DiffEqFlux.jl",
     authors="Chris Rackauckas et al.",
@@ -10,28 +12,7 @@ makedocs(
     format = Documenter.HTML(analytics = "UA-90474609-3",
                              assets = ["assets/favicon.ico"],
                              canonical="https://diffeqflux.sciml.ai/stable/"),
-    pages=[
-        "DiffEqFlux.jl: High Level Scientific Machine Learning (SciML) Pre-Built Architectures" => "index.md",
-        "Differential Equation Machine Learning Tutorials" => Any[
-            "examples/augmented_neural_ode.md",
-            "examples/collocation.md",
-            "examples/hamiltonian_nn.md",
-            "examples/tensor_layer.md",
-            "examples/multiple_shooting.md"
-        ],
-        "Layer APIs" => Any[
-            "Classical Basis Layers" => "layers/BasisLayers.md",
-            "Tensor Product Layer" => "layers/TensorLayer.md",
-            "Continuous Normalizing Flows Layer" => "layers/CNFLayer.md",
-            "Spline Layer" => "layers/SplineLayer.md",
-            "Neural Differential Equation Layers" => "layers/NeuralDELayers.md",
-            "Hamiltonian Neural Network Layer" => "layers/HamiltonianNN.md"
-        ],
-        "Utility Function APIs" => Any[
-            "Smoothed Collocation" => "utilities/Collocation.md",
-            "Multiple Shooting Functionality" => "utilities/MultipleShooting.md",
-        ],
-    ]
+    pages=pages
 )
 
 deploydocs(
