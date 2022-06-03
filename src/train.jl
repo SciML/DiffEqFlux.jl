987cf0c695cbd53dd467d7fac9669d96f5e302d2
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-03 11:35:03 -0400

    Clean up tests for 2.0

--- src/train.jl
+++ src/train.jl
@@ -1,62 +1,9 @@
-"""
-`sciml_train`
-
-### Unconstrained Optimization
-
-```julia
-function sciml_train(loss, _θ, opt = DEFAULT_OPT, adtype = DEFAULT_AD,
-                     _data = DEFAULT_DATA, args...;
-                     callback = (args...) -> false, maxiters = get_maxiters(data),
-                     kwargs...)
-```
-
-### Box Constrained Optimization
-
-```julia
-function sciml_train(loss, θ, opt = DEFAULT_OPT, adtype = DEFAULT_AD,
-                     data = DEFAULT_DATA, args...;
-                     lower_bounds, upper_bounds,
-                     callback = (args...) -> (false), maxiters = get_maxiters(data),
-                     kwargs...)
-```
-
-## Optimizer Choices and Arguments
-
-For a full definition of the allowed optimizers and arguments, please see the
-[Optimization.jl](https://galacticoptim.sciml.ai/dev/) documentation. As
-sciml_train is an interface over Optimization.jl, all of its optimizers and
-arguments can be used from here.
-
-## Loss Functions and Callbacks
-
-Loss functions in `sciml_train` treat the first returned value as the return.
-For example, if one returns `(1.0, [2.0])`, then the value the optimizer will
-see is `1.0`. The other values are passed to the callback function. The callback
-function is `callback(p, args...)` where the arguments are the extra returns from the
-loss. This allows for reusing instead of recalculating. The callback function
-must return a boolean where if `true`, then the optimizer will prematurely end
-the optimization. It is called after every successful step, something that is
-defined in an optimizer-dependent manner.
-
-## Default AD Choice
-
-The current default AD choice is dependent on the number of parameters.
-For <50 parameters both ForwardDiff.jl and Zygote.jl gradients are evaluated
-and the fastest is used. If both methods fail, finite difference method
-is used as a fallback. For ≥50 parameters Zygote.jl is used.
-More refinements to the techniques are planned.
-
-## Default Optimizer Choice
-
-By default, if the loss function is deterministic than an optimizer chain of
-ADAM -> BFGS is used, otherwise ADAM is used (and a choice of maxiters is required).
-"""
 function sciml_train(loss, θ, opt=OptimizationPolyalgorithms.PolyOpt(), adtype=nothing, args...;
                      lower_bounds=nothing, upper_bounds=nothing, cb = nothing,
                      callback = (args...) -> (false),
                      maxiters=nothing, kwargs...)
 
-    @warn "sciml_train is being deprecated in favor of direct usage of Optimization.jl. Please consult the Optimization.jl documentation for more details. Optimization's PolyOpt solver is the polyalgorithm of sciml_train"
+    @warn "sciml_train is being deprecated in favor of direct usage of Optimization.jl. Please consult the Optimization.jl documentation for more details. Optimization.jl's PolyOpt solver is the polyalgorithm of sciml_train"
 
     if adtype === nothing
         if length(θ) < 50
@@ -91,8 +38,7 @@ function sciml_train(loss, θ, opt=OptimizationPolyalgorithms.PolyOpt(), adtype=
     end
 
     optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
-    optfunc = Optimization.instantiate_function(optf, θ, adtype, nothing)
-    optprob = Optimization.OptimizationProblem(optfunc, θ; lb=lower_bounds, ub=upper_bounds, kwargs...)
+    optprob = Optimization.OptimizationProblem(optf, θ; lb=lower_bounds, ub=upper_bounds, kwargs...)
     if maxiters !== nothing
         Optimization.solve(optprob, opt, args...; maxiters, callback = callback, kwargs...)
     else
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,22 +5,18 @@ const is_APPVEYOR = (Sys.iswindows() && haskey(ENV,"APPVEYOR"))
 const is_CI = haskey(ENV,"CI")
 
 @time begin
-if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"
-    @safetestset "Layers Tests" begin include("layers.jl") end
+if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"   
     @safetestset "Fast Layers" begin include("fast_layers.jl") end
-    @safetestset "Optimization Tests" begin include("optimization.jl") end
-    @safetestset "Layers SDE" begin include("layers_sde.jl") end
-    @safetestset "Layers DDE" begin include("layers_dde.jl") end
     @safetestset "hasbranching Overloads" begin include("hasbranching.jl") end
     @safetestset "Collocation Regression" begin include("collocation_regression.jl") end
-    @testset "Distributed" begin include("distributed.jl") end
+    @safetestset "Stiff Nested AD Tests" begin include("stiff_nested_ad.jl") end
 end
 
 if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "BasicNeuralDE"
     @safetestset "Neural DE Tests" begin include("neural_de.jl") end
     @safetestset "Augmented Neural DE Tests" begin include("augmented_nde.jl") end
     #@safetestset "Neural Graph DE" begin include("neural_gde.jl") end
-    @safetestset "Hybrid DE" begin include("hybrid_de.jl") end
+    
     @safetestset "Neural ODE MM Tests" begin include("neural_ode_mm.jl") end
     @safetestset "Fast Neural ODE Tests" begin include("fast_neural_ode.jl") end
     @safetestset "Tensor Product Layer" begin include("tensor_product_test.jl") end
@@ -38,20 +34,9 @@ if GROUP == "Newton"
     @safetestset "Newton Neural ODE Tests" begin include("newton_neural_ode.jl") end
 end
 
-if GROUP == "All" || GROUP == "Integration"
-    @safetestset "Ensemble Tests" begin include("ensembles.jl") end
-    @safetestset "Event Tests" begin include("event_tests.jl") end
-    @safetestset "Partial Neural Tests" begin include("partial_neural.jl") end
-    @safetestset "Size Handling in Adjoint Tests" begin include("size_handling_adjoint.jl") end
-    @safetestset "odenet" begin include("odenet.jl") end
-    @safetestset "GDP Regression Tests" begin include("gdp_regression_test.jl") end
-    @safetestset "Stiff Nested AD Tests" begin include("stiff_nested_ad.jl") end
-end
-end
-
 if !is_APPVEYOR && GROUP == "GPU"
-    @safetestset "odenet GPU" begin include("odenet_gpu.jl") end
     @safetestset "Neural DE GPU Tests" begin include("neural_de_gpu.jl") end
     @safetestset "MNIST GPU Tests: Fully Connected NN" begin include("mnist_gpu.jl") end
     @safetestset "MNIST GPU Tests: Convolutional NN" begin include("mnist_conv_gpu.jl") end
+end
 end
