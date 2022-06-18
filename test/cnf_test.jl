da059d4b3e868ffa4847b5ee74d15082af2ffdb1
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-18 05:50:02 +0200

    Update test/cnf_test.jl
    Co-authored-by: Abhishek Bhatt <46929125+Abhishek-1Bhatt@users.noreply.github.com>

--- test/cnf_test.jl
+++ test/cnf_test.jl
@@ -1,4 +1,4 @@
-using Flux, Distances, Distributions, DistributionsAD, Optimization,
+using DiffEqFlux, Distances, Distributions, DistributionsAD, Optimization,
     LinearAlgebra, OrdinaryDiffEq, Random, Test
 
 Random.seed!(1999)
