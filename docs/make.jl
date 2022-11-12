23ccecaed38b1ce4344be5de354ed549b5d4a75e
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-11-12 02:28:44 +0000

    added reproducibility to index.md and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 using Documenter, DiffEqFlux
 
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
 ENV["GKSwstype"] = "100"
 using Plots
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
