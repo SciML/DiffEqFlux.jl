9ecc3310669fc362af0da32baaa36f72a4926717
Hossein Pourbozorg <prbzrg@gmail.com>
2022-11-21 20:51:30 +0330

    remove quality check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,10 @@
-using DiffEqFlux, Aqua, SafeTestsets, Test
+using DiffEqFlux, SafeTestsets, Test
 
 const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = (Sys.iswindows() && haskey(ENV,"APPVEYOR"))
 const is_CI = haskey(ENV,"CI")
 
 @time begin
-if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Quality"
-    Aqua.test_ambiguities(DiffEqFlux)
-    Aqua.test_unbound_args(DiffEqFlux)
-    Aqua.test_undefined_exports(DiffEqFlux)
-    Aqua.test_project_extras(DiffEqFlux)
-    Aqua.test_stale_deps(DiffEqFlux)
-    Aqua.test_deps_compat(DiffEqFlux)
-    Aqua.test_project_toml_formatting(DiffEqFlux)
-
-    @test isempty(Test.detect_ambiguities(DiffEqFlux; recursive = true))
-    @test isempty(Test.detect_unbound_args(DiffEqFlux; recursive = true))
-end
-
 if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"
     @safetestset "Collocation Regression" begin include("collocation_regression.jl") end
     @safetestset "Stiff Nested AD Tests" begin include("stiff_nested_ad.jl") end
