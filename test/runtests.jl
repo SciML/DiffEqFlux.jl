c63bdc83d735b2f0251095613e11c6ceeb1d069a
Hossein Pourbozorg <prbzrg@gmail.com>
2022-11-22 00:40:52 +0330

    add quality check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,24 @@
-using DiffEqFlux, SafeTestsets, Test
+using DiffEqFlux, Aqua, SafeTestsets, Test
 
 const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = (Sys.iswindows() && haskey(ENV,"APPVEYOR"))
 const is_CI = haskey(ENV,"CI")
 
 @time begin
-if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"   
+if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Quality"
+    Aqua.test_ambiguities(DiffEqFlux)
+    Aqua.test_unbound_args(DiffEqFlux)
+    Aqua.test_undefined_exports(DiffEqFlux)
+    Aqua.test_project_extras(DiffEqFlux)
+    Aqua.test_stale_deps(DiffEqFlux)
+    Aqua.test_deps_compat(DiffEqFlux)
+    Aqua.test_project_toml_formatting(DiffEqFlux)
+
+    @test isempty(Test.detect_ambiguities(DiffEqFlux; recursive = true))
+    @test isempty(Test.detect_unbound_args(DiffEqFlux; recursive = true))
+end
+
+if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "Layers"
     @safetestset "Collocation Regression" begin include("collocation_regression.jl") end
     @safetestset "Stiff Nested AD Tests" begin include("stiff_nested_ad.jl") end
 end
@@ -14,7 +27,7 @@ if GROUP == "All" || GROUP == "DiffEqFlux" || GROUP == "BasicNeuralDE"
     @safetestset "Neural DE Tests" begin include("neural_de.jl") end
     @safetestset "Augmented Neural DE Tests" begin include("augmented_nde.jl") end
     #@safetestset "Neural Graph DE" begin include("neural_gde.jl") end
-    
+
     @safetestset "Neural ODE MM Tests" begin include("neural_ode_mm.jl") end
     @safetestset "Fast Neural ODE Tests" begin include("fast_neural_ode.jl") end
     @safetestset "Tensor Product Layer" begin include("tensor_product_test.jl") end
