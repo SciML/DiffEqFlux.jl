1a7c4d3d80dcb987f9de2f52a591ed678009d78d
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-01-17 23:42:44 -0500

    use a componentarray

--- test/neural_ode_mm.jl
+++ test/neural_ode_mm.jl
@@ -22,7 +22,7 @@ sol = solve(prob,Rodas5(),saveat=0.1)
 
 dudt2 = Lux.Chain(Lux.Dense(3,64,tanh),Lux.Dense(64,2))
 p,st = Lux.setup(rng, dudt2)
-p = Lux.ComponentArrays(p)
+p = Lux.ComponentArray.ComponentArray(p)
 ndae = NeuralODEMM(dudt2, (u,p,t) -> [u[1] + u[2] + u[3] - 1], tspan, M, Rodas5(autodiff=false),saveat=0.1)
 ndae(u₀,p,st)
 
