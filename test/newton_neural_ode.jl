48e844033b91753bfdcbf286aa490ad5f4f03619
Hossein Pourbozorg <prbzrg@gmail.com>
2023-03-09 05:43:24 +0330

    fix newton test

--- test/newton_neural_ode.jl
+++ test/newton_neural_ode.jl
@@ -20,7 +20,7 @@ NN = Flux.Chain(Flux.Dense(n, 5n, tanh),
 @info "ROCK4"
 nODE = NeuralODE(NN, tspan, ROCK4(), reltol=1f-4, saveat=[tspan[end]])
 
-loss_function(θ) = Flux.Losses.mse(y, nODE(x, θ, nODE.st)[end])
+loss_function(θ) = Flux.Losses.mse(y, nODE(x, θ, nODE.st)[1][end])
 l1 = loss_function(nODE.p)
 optf = Optimization.OptimizationFunction((x,p)->loss_function(x), Optimization.AutoZygote())
 optprob = Optimization.OptimizationProblem(optf, nODE.p)
