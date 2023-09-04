8dfd4aa23374688c2a3579b6be14d29e30f447b8
Sathvik Bhagavan <sathvikbhagavan@gmail.com>
2023-09-04 06:09:20 +0000

    test: modify and expand kernel collocation tests

--- test/collocation_regression.jl
+++ test/collocation_regression.jl
@@ -3,25 +3,28 @@ using DiffEqFlux, OrdinaryDiffEq, Test
 function f(u, p, t)
     p .* u
 end
-rc=62
+
+rc = 62
 ps = repeat([-0.001], rc)
-tspan = (7.0, 84.0)
+tspan = (0.0, 50.0)
 u0 = 3.4 .+ ones(rc)
-t = collect(range(minimum(tspan), stop=maximum(tspan), length=157))
+t = collect(range(minimum(tspan), stop = maximum(tspan), length = 1000))
 prob = ODEProblem(f, u0, tspan, ps)
-data = Array(solve(prob, Tsit5(), saveat = t, abstol=1e-12, reltol=1e-12))
-ptest = ones(rc)
+data = Array(solve(prob, Tsit5(), saveat = t, abstol = 1e-12, reltol = 1e-12))
 
-u′,u = collocate_data(data,t,SigmoidKernel())
-@test sum(abs2,u - data) < 1e-8
-
-function loss(p)
-    cost = zero(first(p))
-    for i in 1:size(u′,2)
-      _du = f(@view(u[:,i]),p,t[i])
-      u′i = @view u′[:,i]
-      cost += sum(abs2,u′i .- _du)
-    end
-    sqrt(cost)
+@testset "$kernel" for kernel in [
+    EpanechnikovKernel(),
+    UniformKernel(),
+    TriangularKernel(),
+    QuarticKernel(),
+    TriweightKernel(),
+    TricubeKernel(),
+    CosineKernel(),
+    GaussianKernel(),
+    LogisticKernel(),
+    SigmoidKernel(),
+    SilvermanKernel(),
+]
+    u′, u = collocate_data(data, t, kernel)
+    @test sum(abs2, u - data) < 1e-8
 end
-@test loss(ptest) ≈ 418.3400017500223
