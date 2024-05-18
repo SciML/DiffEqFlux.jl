bbf77067cb60027fb2ef10b55e6b6af364c5aeca
Avik Pal <avikpal@mit.edu>
2024-05-18 00:08:46 -0400

    Add quality assurance tests

---
+++ test/qa_tests.jl
@@ -0,0 +1,13 @@
+@testitem "Aqua Q/A" tags=[:qa] begin
+    using Aqua
+
+    Aqua.test_all(DiffEqFlux; ambiguities=false)
+    Aqua.test_ambiguities(DiffEqFlux; recursive = false)
+end
+
+@testitem "Explicit Imports" tags=[:qa] begin
+    using ExplicitImports
+
+    @test check_no_implicit_imports(DiffEqFlux; skip=(ADTypes, Lux, Base, Core)) === nothing
+    @test check_no_stale_explicit_imports(DiffEqFlux) === nothing
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,24 +13,4 @@ end
 #         @safetestset "CNF Layer Tests" begin
 #             include("cnf_test.jl")
 #         end
-#         @safetestset "Neural Hamiltonian ODE Tests" begin
-#             include("hamiltonian_nn.jl")
-#         end
 #     end
-
-#     if GROUP == "All" || GROUP == "Aqua"
-#         @safetestset "Aqua Q/A" begin
-#             using Aqua, DiffEqFlux, LinearAlgebra
-#             Aqua.find_persistent_tasks_deps(DiffEqFlux)
-#             Aqua.test_ambiguities(DiffEqFlux; recursive = false)
-#             #Aqua.test_deps_compat(DiffEqFlux)
-#             Aqua.test_piracies(DiffEqFlux; treat_as_own = [LinearAlgebra.Tridiagonal])
-#             Aqua.test_project_extras(DiffEqFlux)
-#             Aqua.test_stale_deps(DiffEqFlux)
-#             Aqua.test_unbound_args(DiffEqFlux)
-#             Aqua.test_undefined_exports(DiffEqFlux)
-#             # FIXME: Remove Tridiagonal piracy after
-#             # https://github.com/JuliaDiff/ChainRules.jl/issues/713 is merged!
-#         end
-#     end
-# end
